77d480562a0efe80bfaa410e993ed2e7f098c19b
Felipe Scherer Vicentin <vicentin.py@gmail.com>
2023-02-08 01:28:21 -0300

    First test of new encrypt feature

--- arch.sh
+++ arch.sh
@@ -1,65 +1,207 @@
 #!/bin/sh
 
-espaco () {
-    echo -e "\n\n\n" >&2
+espaco() {
+	printf "\n\n\n\n" >&2
 }
 
-check () {
-    if [ "$2" == "1" ]
-    then
-        echo -n "$1 [Y/n] " >&2
-        read ans
+check() {
+	if [ "$2" = "1" ]; then
+		printf "%s [Y/n] " "$1" >&2
+		read -r ans
 
-        [ "$ans" == "" ] || [ "$ans" == "Y" ] || [ "$ans" == "y" ]
+		[ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]
+	else
+		printf "%s [y/N] " "$1" >&2
+		read -r ans
+
+		! [ "$ans" = "" ] || [ "$ans" = "N" ] || [ "$ans" = "n" ]
+	fi
+}
+
+set_time() {
+	timedatectl set-ntp true
+}
+
+fdisk_nonencrypt_cmds () {
+    total_ram="$(free --giga | awk '/^Mem:/{print $2}')GB"
+
+    echo "g"            # Clear new GPT partition table
+
+    echo "n"            # Create new partition
+    echo "1"            # Partition number 1
+    echo ""             # Default: start at the beginning of disk
+    echo "+512M"        # 512 MiB boot partition
+    echo "t"            # Change partition type
+    echo "1"            # EFI partition
+
+    echo "n"            # Create new partition
+    echo "2"            # Partition number 2
+    echo ""             # Default: start immediately after preceding partition
+    echo "+$total_ram"  # Swap the size of total RAM available
+    echo "t"            # Change partition type
+    echo "19"           # Swap partition
+
+    echo "n"            # Create new partition
+    echo "3"            # Partition number 3
+    echo ""             # Default: start immediately after preceding partition
+    echo ""             # Default: extend partition to end of disk
+
+    echo "p"            # Print the in-memory partition table
+    echo "w"            # Write partition table
+}
+
+fdisk_encrypt_cmds () {
+    echo "g"            # Clear new GPT partition table
+
+    echo "n"            # Create new partition
+    echo "1"            # Partition number 1
+    echo ""             # Default: start at the beginning of disk
+    echo "+512M"        # 512 MiB boot partition
+    echo "t"            # Change partition type
+    echo "1"            # EFI partition
+
+    echo "n"            # Create new partition
+    echo "2"            # Partition number 2
+    echo ""             # Default: start immediately after preceding partition
+    echo ""             # Default: extend partition to end of disk
+
+    echo "p"            # Print the in-memory partition table
+    echo "w"            # Write partition table
+}
+
+encrypt_disk () {
+    main_partition="$1"
+    total_ram="$(free --giga | awk '/^Mem:/{print $2}')GB"
+
+    cryptsetup -c aes-xts-plain64 -y -s 512 luksFormat "$main_partition"
+    cryptsetup luksOpen "$main_partition" lvm
+
+    pvcreate /dev/mapper/lvm
+    vgcreate main /dev/mapper/lvm
+
+
+    lvcreate -L "$total_ram" -n swap main
+    lvcreate -l 100%FREE -n root main
+}
+
+create_fs () {
+    boot_partition="$1"
+    swap_partition="$2"
+    root_partition="$3"
+
+    mkfs.fat -F32 -n BOOT "$boot_partition"
+    mkswap -L swap "$swap_partition"
+    mkfs.ext4 -L root "$root_partition"
+}
+
+mount_partitions () {
+    boot_partition="$1"
+    swap_partition="$2"
+    root_partition="$3"
+
+    mount "$root_partition" /mnt
+
+    mkdir -p /mnt/boot/EFI
+    mount "$boot_partition" /mnt/boot/EFI
+
+    swapon -L swap
+}
+
+run_pacstrap() {
+    processor="$1"
+    encrypt=$2
+
+	sudo pacman --noconfirm -Sy archlinux-keyring
+
+    if $encrypt; then
+	    pacstrap /mnt base base-devel linux linux-firmware vim git lsb-release accountsservice grub "$processor-ucode" mkinitcpio lvm2
     else
-        echo -n "$1 [y/N] " >&2
-        read ans
-
-        ! [ "$ans" == "" ] || [ "$ans" == "N" ] || [ "$ans" == "n" ]
+	    pacstrap /mnt base base-devel linux linux-firmware vim git lsb-release accountsservice grub "$processor-ucode"
     fi
 }
 
-set_time () {
-    timedatectl set-ntp true
+create_fstab() {
+	genfstab -U /mnt >>/mnt/etc/fstab
 }
 
-run_pacstrap () {
-    sudo pacman --noconfirm -Sy archlinux-keyring
-    pacstrap /mnt base base-devel linux linux-firmware vim git lsb-release accountsservice
-}
+setup_arch() {
+	git clone https://github.com/Vinschers/linux-install.git
+	chmod -R 777 linux-install/
+	cd linux-install/arch/ || exit 1
 
-create_fstab () {
-    genfstab -U /mnt >> /mnt/etc/fstab
-}
-
-setup_arch () {
-    git clone https://github.com/Vinschers/linux-install.git
-    chmod -R 777 linux-install/
-    cd linux-install/arch/
-
-    echo -e 'Starting setup...\n\n' &&
-    ./setup.sh "$1" "$2" "$3" "$4" "$5"
+	printf 'Starting setup...\n\n\n' && ./setup.sh "$1" "$2" "$3" "$4" "$5"
 }
 
 check "Debug mode?" 0 && debug=true || debug=false
+
+encrypt=false
+
+if check "Create partitions?" 1; then
+    printf "Device: "
+    read -r device
+
+    if check "Encrypt disk" 1; then
+        fdisk_encrypt_cmds | fdisk "$device"
+
+        main_partition="$(lsblk -o KNAME -n "$device" | sed -n '3p')"
+        encrypt_disk "$main_partition"
+
+        boot_partition="$(lsblk -o KNAME -n "$device" | sed -n '2p')"
+        swap_partition="/dev/mapper/main-swap"
+        root_partition="/dev/mapper/main-root"
+
+        encrypt=true
+    else
+        fdisk_nonencrypt_cmds | fdisk "$device"
+
+        boot_partition="$(lsblk -o KNAME -n "$device" | sed -n '2p')"
+        swap_partition="$(lsblk -o KNAME -n "$device" | sed -n '3p')"
+        root_partition="$(lsblk -o KNAME -n "$device" | sed -n '4p')"
+    fi
+else
+    printf "Boot partition: "
+    read -r boot_partition
+
+    printf "Swap partition: "
+    read -r swap_partition
+
+    printf "Root partition: "
+    read -r root_partition
+fi
+espaco
+
 check "VirtualBox environment?" 0 && vm=true || vm=false
 
-echo -n "Boot partition: "
-read boot_partition
+printf "Hostname: "
+read -r hostname
 
-echo -n "Hostname: "
-read hostname
+printf "User name: "
+read -r user_name
 
-echo -n "User name: "
-read user_name
+printf "Processor (intel/amd): "
+read -r processor
 
-! $debug || check "Setup time?" 1 && set_time ; espaco
 
-! $debug || check "Run pacstrap command?" 1 && run_pacstrap ; espaco
+! $debug || check "Create filesystems" 1 && create_fs "$boot_partition" "$swap_partition" "$root_partition"
+espaco
 
-! $debug || check "Create fstab file?" 1 && create_fstab ; espaco
+
+! $debug || check "Mount partitions" 1 && mount_partitions "$boot_partition" "$swap_partition" "$root_partition"
+espaco
+
+
+! $debug || check "Setup time?" 1 && set_time
+espaco
+
+
+! $debug || check "Run pacstrap command?" 1 && run_pacstrap "$processor" $encrypt
+espaco
+
+! $debug || check "Create fstab file?" 1 && create_fstab
+espaco
 
 export -f setup_arch
-! $debug || check "Clone full repository and run arch setup?" 1 && arch-chroot /mnt /bin/sh -c "setup_arch '$debug' '$boot_partition' '$hostname' '$user_name' '$vm'"; espaco
+! $debug || check "Clone full repository and run arch setup?" 1 && arch-chroot /mnt /bin/sh -c "setup_arch '$debug' '$main_partition' '$hostname' '$user_name' '$vm'"
+espaco
 
 echo "Reboot the computer and remove the installation media."
--- arch/setup.sh
+++ arch/setup.sh
@@ -1,111 +1,118 @@
 #!/bin/sh
 
-espaco () {
-    echo -e "\n\n\n" >&2
+espaco() {
+	printf "\n\n\n\n" >&2
 }
 
-check () {
-    if [ "$2" == "1" ]
-    then
-        echo -n "$1 [Y/n] " >&2
-        read ans
+check() {
+	if [ "$2" = "1" ]; then
+		printf "%s [Y/n] " "$1" >&2
+		read -r ans
 
-        [ "$ans" == "" ] || [ "$ans" == "Y" ] || [ "$ans" == "y" ]
-    else
-        echo -n "$1 [y/N] " >&2
-        read ans
+		[ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]
+	else
+		printf "%s [y/N] " "$1" >&2
+		read -r ans
 
-        ! [ "$ans" == "" ] || [ "$ans" == "N" ] || [ "$ans" == "n" ]
-    fi
+		! [ "$ans" = "" ] || [ "$ans" = "N" ] || [ "$ans" = "n" ]
+	fi
 }
 
-setup_vb () {
-    pacman -S --noconfirm virtualbox-guest-utils
-    systemctl enable vboxservice
+setup_vb() {
+	pacman -S --noconfirm virtualbox-guest-utils
+	systemctl enable vboxservice
 }
 
-setup_time_zone () {
-    ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
-    hwclock --systohc
+setup_time_zone() {
+	ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
+	hwclock --systohc
 }
 
-setup_network () {
-    pacman -S --noconfirm networkmanager
-    systemctl enable NetworkManager
+setup_network() {
+	pacman -S --noconfirm networkmanager
+	systemctl enable NetworkManager
 
-    echo "$1" > /etc/hostname
+	echo "$1" >/etc/hostname
 
-    echo -e "127.0.0.1        localhost\n" > /etc/hosts
-    echo -e "::1              localhost\n" >> /etc/hosts
-    echo -e "127.0.1.1        $1.localdomain  $1" >> /etc/hosts
+	echo -e "127.0.0.1        localhost\n" >/etc/hosts
+	echo -e "::1              localhost\n" >>/etc/hosts
+	echo -e "127.0.1.1        $1.localdomain  $1" >>/etc/hosts
 }
 
-setup_grub () {
-    pacman -S --noconfirm grub parted
-    espaco
-    echo "Setting up grub menu..."
+setup_grub() {
+    main_partition="$1"
 
-    boot_partition=$1
+    [ -n "$main_partition" ] && sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=$main_partition:main root=/dev/mapper/main-root /g" /etc/default/grub
 
-    device=$(lsblk -no pkname $boot_partition)
-    device="/dev/$device"
+	echo "Setting up grub menu..."
 
-    partition_label=$(parted $device print | grep -i "Partition Table")
-    partition_label=${partition_label##* }
+	pacman -S --noconfirm efibootmgr dosfstools os-prober mtools
+	grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 
-    if [ "$partition_label" == "gpt" ]
-    then
-        mkdir -p /boot/EFI
-        mount $boot_partition /boot/EFI
+	echo "GRUB_DISABLE_OS_PROBER=false" >>/etc/default/grub
 
-        pacman -S --noconfirm efibootmgr dosfstools os-prober mtools
-        grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-	
-	echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    elif [ "$partition_label" == "msdos" ]
-    then
-        mount $boot_partition /boot
-        grub-install $device
-    fi
-
-    pacman -S --noconfirm ntfs-3g
-    grub-mkconfig -o /boot/grub/grub.cfg
+	pacman -S --noconfirm ntfs-3g
+	grub-mkconfig -o /boot/grub/grub.cfg
 }
 
-setup_locale () {
-    mv ./locale.gen /etc/locale.gen
-    locale-gen
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+setup_locale() {
+	mv ./locale.gen /etc/locale.gen
+	locale-gen
+	echo "LANG=en_US.UTF-8" >/etc/locale.conf
 }
 
-create_new_user () {
-    useradd -mg wheel $user_name
-    passwd $user_name
+create_new_user() {
+	useradd -mg wheel $user_name
+	passwd $user_name
 
-    mv /home/$user_name/.bashrc /home/$user_name/.bashrc.old
-    cp ./bashrc.sh /home/$user_name/.bashrc
+	mv /home/$user_name/.bashrc /home/$user_name/.bashrc.old
+	cp ./bashrc.sh /home/$user_name/.bashrc
+}
+
+change_mkinitcpio () {
+    default_modules="$(grep "^MODULES=" /etc/mkinitcpio.conf)"
+    modified_modules="${default_modules%?} ext4)"
+    modified_modules="$(echo "$modified_modules" | sed 's/( /(/g')"
+
+    sed -i "s/^$default_modules/$modified_modules/g" /etc/mkinitcpio.conf
+
+    default_hooks="$(grep "^HOOKS=" /etc/mkinitcpio.conf)"
+    modified_hooks="${default_hooks%?} encrypt lvm2)"
+    modified_hooks="$(echo "$modified_hooks" | sed 's/( /(/g')"
+
+    sed -i "s/^$default_hooks/$modified_hooks/g" /etc/mkinitcpio.conf
+
+    mkinitcpio -p linux
 }
 
 debug="$1"
-boot_partition="$2"
+main_partition="$2"
 hostname="$3"
 user_name="$4"
 vm="$5"
 
+$vm && setup_vb
+espaco
 
-$vm && setup_vb ; espaco
+! $debug || check "Setup localtime?" 1 && setup_time_zone
+espaco
 
+! $debug || check "Setup locale?" 1 && setup_locale
+espaco
 
-! $debug || check "Setup localtime?" 1 && setup_time_zone ; espaco
+! $debug || check "Setup Network configuration?" 1 && setup_network "$hostname"
+espaco
 
-! $debug || check "Setup locale?" 1 && setup_locale ; espaco
+! $debug || check "Change root password?" 1 && passwd
+espaco
 
-! $debug || check "Setup Network configuration?" 1 && setup_network "$hostname" ; espaco
+! $debug || check "Update sudoers file?" 1 && cat ./sudoers >/etc/sudoers
+espaco
 
-! $debug || check "Change root password?" 1 && passwd ; espaco
+! $debug || check "Create new user?" 1 && create_new_user
+espaco
 
-! $debug || check "Update sudoers file?" 1 && cat ./sudoers > /etc/sudoers ; espaco
+! $debug || check "Install and setup grub?" 1 && setup_grub "$main_partition"
+espaco
 
-! $debug || check "Create new user?" 1 && create_new_user ; espaco
-
-! $debug || check "Install and setup grub?" 1 && setup_grub $boot_partition
+[ -n "$main_partition" ] && change_mkinitcpio
