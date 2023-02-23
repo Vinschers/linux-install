49a4db82de6b3a924c223797003c45c6945ce462
Scherer <42344207+Vinschers@users.noreply.github.com>
2023-02-23 06:05:28 +0000

    Update bashrc.sh

--- arch/bashrc.sh
+++ arch/bashrc.sh
@@ -24,7 +24,7 @@ install_yay() {
 
 download_dotfiles() {
     mkdir -p "$HOME/.config/dotfiles"
-	git clone --bare https://github.com/Vinschers/dotfiles.git "$HOME/.config/dotfiles/.dotfiles-git"
+	git clone --bare git@github.com:Vinschers/dotfiles.git "$HOME/.config/dotfiles/.dotfiles-git"
 	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} rm "$HOME/{}"
 	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" checkout
 	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" config --local status.showUntrackedFiles no
