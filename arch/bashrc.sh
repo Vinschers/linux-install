edf90a1f8c915dd3e2d7f32b3293a1d8fbcc8ab0
Felipe <github.k9qcx@slmail.me>
2023-07-01 18:44:05 -0300

    Change bashrc on arch install

--- arch/bashrc.sh
+++ arch/bashrc.sh
@@ -14,27 +14,8 @@ check() {
 	fi
 }
 
-install_yay() {
-	git clone https://aur.archlinux.org/yay.git
-	cd yay/ || exit 1
-	makepkg -si
-	cd ..
-	rm -rf yay/
+setup() {
+	curl -s "https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh" | /bin/sh
 }
 
-download_dotfiles() {
-	mkdir -p "$HOME/.config/dotfiles"
-	git clone --bare https://github.com/Vinschers/dotfiles.git "$HOME/.config/dotfiles/.dotfiles-git"
-	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} rm "$HOME/{}"
-	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" checkout
-	git --git-dir="$HOME/.config/dotfiles/.dotfiles-git/" --work-tree="$HOME" config --local status.showUntrackedFiles no
-
-	rm "$HOME/.bash_profile" "$HOME/.bashrc.old"
-
-	source "$HOME/.profile"
-	check "Run setup script?" 1 && "$SCRIPTS_DIR/setup/setup.sh" && rm -f "$HOME/.bashrc"
-}
-
-check "Install yay?" 1 && install_yay
-
-check "Download dotfiles?" 1 && download_dotfiles
+check "Run setup script?" 1 && setup
