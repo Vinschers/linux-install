699764e55f63cedf4955f5fdee16354c95ea5b4c
Vinschers <github.90eel@slmail.me>
2023-07-03 18:53:43 -0300

    Make setup.sh executable in bashrc

--- arch/bashrc.sh
+++ arch/bashrc.sh
@@ -5,6 +5,7 @@ read -r setup
 
 if [ "$setup" = "" ] || [ "$setup" = "Y" ] || [ "$setup" = "y" ]; then
     curl -O https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh
+    chmod +x setup.sh
     ./setup.sh
     rm -f ./setup.sh
 fi
