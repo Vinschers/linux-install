764b5bf49250aed3b5ee77f37ddb836df1726f9e
Vinschers <github.90eel@slmail.me>
2023-07-03 15:29:41 -0300

    Downloading setup script

--- arch/bashrc.sh
+++ arch/bashrc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-printf "Run setup script? [Y/n]"
+printf "Run setup script? [Y/n] "
 read -r setup
 
 if [ "$setup" = "" ] || [ "$setup" = "Y" ] || [ "$setup" = "y" ]; then
-    curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh | sh
+    curl -O https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh
+    ./setup.sh
+    rm -f ./setup.sh
 fi
