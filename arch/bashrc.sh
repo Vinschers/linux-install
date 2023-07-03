eaaf9cd785991f892aa31defc7263732d134a6bf
Vinschers <github.90eel@slmail.me>
2023-07-03 13:38:59 -0300

    Update bashrc.sh

--- arch/bashrc.sh
+++ arch/bashrc.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-check() {
-	if [ "$2" == "1" ]; then
-		echo -n "$1 [Y/n] " >&2
-		read ans
+printf "Run setup script? [Y/n]"
+read -r setup
 
-		[ "$ans" == "" ] || [ "$ans" == "Y" ] || [ "$ans" == "y" ]
-	else
-		echo -n "$1 [y/N] " >&2
-		read ans
-
-		! [ "$ans" == "" ] || [ "$ans" == "N" ] || [ "$ans" == "n" ]
-	fi
-}
-
-setup() {
-	curl -O "https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh"
-    chmod +x setup.sh
-    ./setup.sh
-}
-
-check "Run setup script?" 1 && setup
+if [ "$setup" = "" ] || [ "$setup" = "Y" ] || [ "$setup" = "y" ]; then
+    curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/setup.sh | sh
+fi
